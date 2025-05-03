2cdd14cab2a620675e4371981ef50b49a140ba63
prasannakumar <prasannakumarsinganamalla@gmail.com>
2025-05-04 00:38:11 +0530

    add response model to Posts endpoints

--- APP/schema.py
+++ APP/schema.py
@@ -12,4 +12,14 @@ class post_data(PYDANTIC_MODEL):
     post_name: str
     description: str
     published: bool
+
+class retrive_data(post_data):
+    """
+    Pydantic model to define the structure of user data.
+    This ensures that the data coming to the API is validated and properly formatted.
+    """
+    id: int
+     
+    class Config:
+        orm_mode = True    
     
