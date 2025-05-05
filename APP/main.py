dda867d5312e36ef6b8be55e573f3dc044a51391
prasannakumar <prasannakumarsinganamalla@gmail.com>
2025-05-05 12:45:56 +0530

    add auth router

--- APP/main.py
+++ APP/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI # type: ignore
-from database import Base, engine
+from database import engine
 import models
-from routers import post , users
-
+from routers import post, users, auth
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(post.router)
 app.include_router(users.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def main_func():
