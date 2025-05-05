1a3a67668905a37a286dc90200e4a0ad4a726631
prasannakumar <prasannakumarsinganamalla@gmail.com>
2025-05-05 12:47:06 +0530

    add password hash verify func

--- APP/utils.py
+++ APP/utils.py
@@ -6,4 +6,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash(password: str):
     hashedvalue = pwd_context.hash(password)
     return hashedvalue
-    
+
+
+def verify(original_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(original_password, hashed_password)
