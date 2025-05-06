dc36ed687895b4d88282fa4c4e04e4be4fef00ee
prasannakumar <142010251+spkumar17@users.noreply.github.com>
2025-05-07 00:13:53 +0530

    Update Oauth2.py

--- APP/Oauth2.py
+++ APP/Oauth2.py
@@ -21,7 +21,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
     
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
